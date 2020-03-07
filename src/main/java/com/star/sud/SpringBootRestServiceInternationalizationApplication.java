5031e2bf91da3c29d9b6dce0fb861cc8aab685c6
Sudarshan <sudarshan.java.p@gmail.com>
2020-03-07 17:38:38 +0800

    Base Release

---
+++ src/main/java/com/star/sud/SpringBootRestServiceInternationalizationApplication.java
@@ -0,0 +1,34 @@
+package com.star.sud;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@SpringBootApplication
+public class SpringBootRestServiceInternationalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServiceInternationalizationApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver getLocaleResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+
+	}
+
+}
